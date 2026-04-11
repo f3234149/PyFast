973d77f1b24f2ddc780e60cb5eca62ce6360f42c
f3234149 <hancug@163.com>
2026-04-11 23:39:20 +0800

    CORS中间件

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from routers import user
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # 允许的源， 生产环境需要指定源
+    allow_credentials=True, # 允许携带cookie
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
